e57ba5e7ccb8bc3854f4757e94fa80f1fcbf31a1
c00b3r <sanuyOKmaklaren0>
2024-09-12 22:16:31 +0500

    feat: implement cansel handler for order

--- src/components/OrderCard/OrderCard.tsx
+++ src/components/OrderCard/OrderCard.tsx
@@ -1,66 +1,113 @@
 import { useState } from "react";
 import { Order } from "../../interface";
 import { Box, Button, Card, CardActions, CardContent, Collapse, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 interface OrderCardPros {
   order: Order;
 }
 
+type completeOrder = 4 | 5 | 6;
+
 export default function OrderCard({ order }: OrderCardPros) {
   const [open, setOpen] = useState(false);
+  const [localOrder, setLocalOrder] = useState(order);
+
+  const canselOrderHandler = async (orderId: string, status: completeOrder) => {
+    try {
+      const response = await fetch(`http://localhost:3000/orders/${orderId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          status: status,
+        }),
+      });
+      if (response.ok) {
+        setLocalOrder({ ...localOrder, status });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleToggleItems = () => {
     setOpen(!open);
   };
 
   const renderStatus = (status: number) => {
-    switch (status) {
-      case 0:
-        return "Создан";
-      case 1:
-        return "Оплачен";
-      case 2:
-        return "В пути";
-      case 3:
-        return "Доставлен на точку";
-      case 4:
-        return "Получен";
-      case 5:
-        return "Архивирован";
-      case 6:
-        return "Возврат";
-      default:
-        return "Неизвестный статус";
-    }
+    const statuses: { [key: number]: string } = {
+      0: "Создан",
+      1: "Оплачен",
+      2: "В пути",
+      3: "Доставлен на точку",
+      4: "Получен",
+      5: "Архивирован",
+      6: "Возврат",
+    };
+    return statuses[status] || "Неизвестный статус";
   };
   return (
     <Card sx={{ marginBottom: "16px", padding: "16px", boxShadow: 3 }}>
       <CardContent>
-        <Typography variant="h6">Номер заказа: {order.id}</Typography>
-        <Typography>Дата создания: {new Date(order.createdAt).toLocaleDateString()}</Typography>
-        <Typography>Статус: {renderStatus(order.status)}</Typography>
-        <Typography>Сумма заказа: {order.total}₽</Typography>
-        <Typography>Количество товаров: {order.items.length}</Typography>
+        <Typography variant="h6">Номер заказа: {localOrder.id}</Typography>
+        <Typography>
+          Дата создания: {new Date(localOrder.createdAt).toLocaleDateString()}
+        </Typography>
+        <Typography>Статус: {renderStatus(localOrder.status)}</Typography>
+        <Typography>Сумма заказа: {localOrder.total}₽</Typography>
+        <Typography>Количество товаров: {localOrder.items.length}</Typography>
       </CardContent>
       <CardActions>
-        <Button variant="contained" color="primary" onClick={handleToggleItems}>
-          {open ? "Скрыть товары" : "Показать все товары"}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleToggleItems}
+          sx={{ width: "174px" }}
+        >
+          {open ? "Скрыть товары" : "Показать товары"}
         </Button>
         <Button
           variant="contained"
           color="error"
-          disabled={order.status !== 0}
-          onClick={() => console.log("Завершить заказ")}
+          disabled={[4, 5, 6].includes(localOrder.status)}
+          onClick={() => canselOrderHandler(localOrder.id, 4)}
         >
           Завершить заказ
+        </Button>
+        <Button
+          variant="contained"
+          color="warning"
+          disabled={[4, 5, 6].includes(localOrder.status)}
+          onClick={() => canselOrderHandler(localOrder.id, 5)}
+        >
+          Архивировать
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          disabled={[4, 5, 6].includes(localOrder.status)}
+          onClick={() => canselOrderHandler(localOrder.id, 6)}
+        >
+          Возврат
         </Button>
       </CardActions>
       <Collapse in={open} timeout="auto" unmountOnExit>
         <Box sx={{ marginTop: "16px" }}>
           {order.items.map((item, index) => (
-            <Typography key={index}>
+            <NavLink
+              to={`/advertisements/${item.id}`}
+              style={{
+                textDecoration: "none",
+                display: "flex",
+                flexDirection: "column",
+                color: "gray",
+              }}
+              key={index}
+            >
               {item.name} - {item.count} шт.
-            </Typography>
+            </NavLink>
           ))}
         </Box>
       </Collapse>
